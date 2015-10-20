18e98f1d7fdb98436fceca8c314f57569c3b785d
Rachana Acharya <rasnacharya@gmail.com>
2015-10-20 02:21:10 -0600

    Formatted lines

--- reverseString.js
+++ reverseString.js
@@ -1,7 +1,7 @@
 function reverseString(str) {
-  strArray = str.split('');//splits str by characters
+  strArray = str.split(''); //splits str by characters
   strArray.reverse();
-  str = strArray.join("");//joins by characters
+  str = strArray.join(""); //joins by characters
   return str;
 }
 
