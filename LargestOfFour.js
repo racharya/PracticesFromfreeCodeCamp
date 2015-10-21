b9b879f58eb0c9b278dba0bf641423487c3b1222
Rachana Acharya <rasnacharya@gmail.com>
2015-10-20 23:05:30 -0600

    Formatted the contents

--- LargestOfFour.js
+++ LargestOfFour.js
@@ -2,14 +2,14 @@ function largestOfFour(arr) {
   // You can do this!
   retArray = [];
   var compVal = 0;
-  for(var i = 0; i < arr.length; i++){
+  for (var i = 0; i < arr.length; i++) {
     subArray = arr[i];
 
     console.log(subArray);
     compVal = 0;
-    for( var j = 0; j < subArray.length; j++){
+    for (var j = 0; j < subArray.length; j++) {
 
-      if(subArray[j] > compVal){
+      if (subArray[j] > compVal) {
         compVal = subArray[j];
         console.log(compVal);
       }
@@ -21,4 +21,9 @@ function largestOfFour(arr) {
   return retArray;
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1]
+]);
